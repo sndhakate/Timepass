e70852267e6fc35a274084cde9b34f956fa817b7
sndhakate <47688808+sndhakate@users.noreply.github.com>
2019-04-16 22:39:25 +0530

    element enter in the hashmap

--- CollectionPractice/src/com/collection/practice/HashMapDemo.java
+++ CollectionPractice/src/com/collection/practice/HashMapDemo.java
@@ -1,9 +1,19 @@
 package com.collection.practice;
 
+import java.util.HashMap;
+
 public class HashMapDemo {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("I love TroubleShooting");
+		
+		HashMap<Object, Object> hm = new HashMap<>();
+		
+		hm.put(101, "SWapnil");
+		hm.put(102, "Loves");
+		hm.put(103, "Troubleshooting");
+		
+		System.out.println(hm);
 		
 	}
 	
