e4363faf80527b127fa42560bdbb5ac79eef63e9
sndhakate <47688808+sndhakate@users.noreply.github.com>
2019-04-16 22:33:36 +0530

    Update HashMapDemo.java
    some minor changes

--- CollectionPractice/src/com/collection/practice/HashMapDemo.java
+++ CollectionPractice/src/com/collection/practice/HashMapDemo.java
@@ -3,6 +3,7 @@ package com.collection.practice;
 public class HashMapDemo {
 	public static void main(String[] args) {
 		System.out.println("Hello");
+		System.out.println("I love TroubleShooting");
 		
 	}
 	
