874d6ecbb63e8b7447e89ceec3eeebbd735f4c94
sndhakate <47688808+sndhakate@users.noreply.github.com>
2019-04-17 14:58:31 +0530

    with object array

---
+++ CollectionPractice/src/com/collection/practice/ArraysDemo.java
@@ -0,0 +1,20 @@
+package com.collection.practice;
+
+public class ArraysDemo {
+	
+	public static void main(String[] args) {
+		
+		Object obj [] = new Object [3];
+		
+		obj [0] = "Swapnil";
+		obj [1] = 102;
+		obj [2] = 10.20;
+		
+		System.out.println(obj[0]);
+		
+		
+		
+		
+	}
+
+}
